a80c84141d7922ed76c775f36a3a8e05d93c4c28
bharathandileep <bharathanoffice@gmail.com>
2025-03-03 14:28:22 +0530

    new features/dev branch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -303,7 +303,7 @@ const organizationsAppRoutes = {
       element: <ListOrganizations />,
       route: PrivateRoute,
     },
-    {
+    { 
       path: "/apps/organizations/edit/:id",
       name: "Edit Organizations",
       element: <EditOrganizations />,
